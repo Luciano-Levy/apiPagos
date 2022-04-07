ccf17b60cb21738d302b28c2f03a96fbbdd99438
Luciano Levy <lucianolevy03@gmail.com>
2022-04-06 23:18:27 -0300

    init

---
+++ src/test/java/sistemapagos/apiPagos/ApiPagosApplicationTests.java
@@ -0,0 +1,13 @@
+package sistemapagos.apiPagos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPagosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
